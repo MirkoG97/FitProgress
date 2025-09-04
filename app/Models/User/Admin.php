b9f26bd6f79859c5989fefbf54c14f475f32868b
Mirko Giraldo <mirkogiraldo@gmail.com>
2025-09-04 23:11:36 +0200

    Sistemazione cartelle e installazione Composer

---
+++ app/Models/User/Admin.php
@@ -0,0 +1,8 @@
+<?php
+    class Admin extends Utente{
+
+        function __construct($nome, $cognome, $mail, $password, $ruolo, $id = null, $personal_trainer = null){
+            parent::__construct($nome, $cognome, $mail, $password, $ruolo, $id, $personal_trainer);
+        }
+    }
+?>
--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -150,46 +150,4 @@
             }
         }
     }
-
-    class Personal_Trainer extends Utente{
-
-        function __construct($nome, $cognome, $mail, $password, $ruolo, $id = null, $personal_trainer = null){
-            parent::__construct($nome, $cognome, $mail, $password, $ruolo, $id, $personal_trainer);
-        }
-
-        function getClientiDB($id_personal_trainer){
-            global $connessione; 
-            $query = "  SELECT * 
-                        FROM utente
-                        WHERE idPersonalTrainer = ?";
-            $stmt = $connessione->prepare($query);
-            $stmt->execute([$id_personal_trainer]);
-
-            $result = $stmt->get_result();
-
-            $clienti = [];
-
-            if ($result->num_rows > 0) {
-                while ($cliente = $result->fetch_assoc()) {
-                    $clienti[] = $cliente;
-                }
-            }
-
-            //var_dump($clienti);
-
-            if (!empty($clienti)) {
-                return $clienti;
-            } else {
-                return null;
-            }
-
-        }
-    }
-
-    class Admin extends Utente{
-
-        function __construct($nome, $cognome, $mail, $password, $ruolo, $id = null, $personal_trainer = null){
-            parent::__construct($nome, $cognome, $mail, $password, $ruolo, $id, $personal_trainer);
-        }
-    }
 ?>
