344a378f701b810b579528eda0d7785f48a6f3e4
Mirko Giraldo <mirkogiraldo@gmail.com>
2026-02-02 23:28:24 +0100

    Ho inserito creato una nuova migration per l'assegnazione di un utente al personal trainer

---
+++ database/migrations/2026_02_02_221846_create_user_trainer_assignment.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_trainer_assignment', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('personal_trainer_id');
+            $table->foreign('personal_trainer_id')->references('id')->on('personal_trainers')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_trainer_assignment');
+    }
+};
